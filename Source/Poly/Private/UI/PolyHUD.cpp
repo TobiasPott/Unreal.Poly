764bd1215c7646c47b0e8433918e79e076a4f10e
Tobias Pott <mailme@tobiaspott.de>
2024-04-17 18:15:58 +0200

    Added marquee one shot selector to gather multiple objects at once

---
+++ Source/Poly/Private/UI/PolyHUD.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/PolyHUD.h"
+#include "GameFramework/HUD.h"
+#include "GenericPlatform/GenericApplication.h"
+#include "Misc/App.h"
+#include "EngineGlobals.h"
+#include "Layout/Margin.h"
+#include "Engine/Engine.h"
+#include "GameFramework/GameModeBase.h"
+#include "EngineUtils.h"
+#include "Engine/Canvas.h"
+#include "Misc/UObjectToken.h"
+#include "UObject/UObjectIterator.h"
+
+
+APolyHUD::APolyHUD()
+{
+
+}
+
---
+++ Source/Poly/Public/UI/PolyHUD.h
@@ -0,0 +1,89 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "PolyHUD.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class POLY_API APolyHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	APolyHUD();
+
+};
+
+
+
+UCLASS(Blueprintable)
+class POLY_API UMarqueeSelectionRequest : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Selection")
+	FVector2D FirstPoint = FVector2D(0, 0);
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Selection")
+	FVector2D SecondPoint = FVector2D(0, 0);
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Selection")
+	bool bIncludeNonCollider = false;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Selection")
+	bool bOnlyEnclosed = false;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Selection")
+	TSubclassOf<AActor> FilterClass;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Selection")
+	TArray<AActor*> Actors;
+
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Selection")
+	void Init(const FVector2D& InFirstPoint, const FVector2D& InSecondPoint, TSubclassOf<AActor> InFilterClass, bool bInIncludeNonCollider = false, bool bInOnlyEnclosed = false)
+	{
+		this->FirstPoint = InFirstPoint;
+		this->SecondPoint = InSecondPoint;
+		this->bIncludeNonCollider = bInIncludeNonCollider;
+		this->bOnlyEnclosed = bInOnlyEnclosed;
+		this->FilterClass = InFilterClass;
+	}
+
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Selection")
+	bool IsEmpty() { return Actors.IsEmpty(); }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Selection")
+	bool IsNotEmpty() { return !Actors.IsEmpty(); }
+
+	UFUNCTION(BlueprintCallable, Category = "Selection")
+	void OnFinished() 
+	{
+		if (Finished.IsBound())
+			Finished.Broadcast(this, !Actors.IsEmpty());
+	}
+
+
+	UFUNCTION(BlueprintCallable, Category = "Selection")
+	void SetActors(TArray<AActor*> InActors)
+	{
+		Actors.Reset(InActors.Num());
+		for (int i = 0; i < InActors.Num(); i++)
+			Actors.Add(InActors[i]);
+	}
+
+
+public:
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FMarqueeSelectionRequestFinished, UMarqueeSelectionRequest*, Request, bool, bSuccess);
+	UPROPERTY(BlueprintAssignable, EditDefaultsOnly, Category = "Default")
+	FMarqueeSelectionRequestFinished Finished;
+
+};
