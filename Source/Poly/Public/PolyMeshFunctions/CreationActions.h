5f3c1aaf8cfcc31b10df84326899a486a33971e2
Tobias Pott <mailme@tobiaspott.de>
2024-04-28 23:20:33 +0200

    Added sphere, cylinder and conde shape actions

--- Source/Poly/Public/PolyMeshFunctions/CreationActions.h
+++ Source/Poly/Public/PolyMeshFunctions/CreationActions.h
@@ -7,11 +7,31 @@
 #include "Actions/ActionBase.h"
 #include "CreationActions.generated.h"
 
+
+
+UCLASS()
+class POLY_API UCreateShapeActionBase : public UActionBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	UCreateShapeActionBase()
+	{
+		Description = "poly.CreateShape";
+		ShortName = "Create Shape";
+	}
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	FVector Offset = FVector::ZeroVector;
+
+
+};
 /**
  *
  */
 UCLASS(Blueprintable)
-class POLY_API UCreateBoxAction: public UActionBase
+class POLY_API UCreateBoxAction: public UCreateShapeActionBase
 {
 	GENERATED_BODY()
 
@@ -24,10 +44,104 @@ public:
 	}
 
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
-	FVector Offset = FVector::ZeroVector;
+	FVector Dimensions = FVector(100, 100, 100);
+
+public:
+	bool Execute_Implementation(bool bEmitRecord) override;
+};
+
+
+UCLASS(Blueprintable)
+class POLY_API UCreateSphereAction : public UCreateShapeActionBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	UCreateSphereAction()
+	{
+		Description = "poly.CreateSphere";
+		ShortName = "Create Sphere";
+	}
 
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
-	FVector Dimensions = FVector(100, 100, 100);
+	float Radius = 50.0f;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	int32 StepX = 6;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	int32 StepY = 6;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	int32 StepZ = 6;
+
+
+public:
+	bool Execute_Implementation(bool bEmitRecord) override;
+};
+
+
+
+UCLASS(Blueprintable)
+class POLY_API UCreateCylinderAction : public UCreateShapeActionBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	UCreateCylinderAction()
+	{
+		Description = "poly.CreateCylinder";
+		ShortName = "Create Cylinder";
+	}
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	float Radius = 50.0f;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	float Height = 100.0f;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	int32 RadialSteps = 12;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	int32 HeightSteps = 0;
+
+
+public:
+	bool Execute_Implementation(bool bEmitRecord) override;
+};
+
+
+UCLASS(Blueprintable)
+class POLY_API UCreateConeAction : public UCreateShapeActionBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	UCreateConeAction()
+	{
+		Description = "poly.CreateCone";
+		ShortName = "Create Cone";
+	}
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	float BaseRadius = 50.0f;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	float TopRadius = 5.0;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	float Height = 100;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	int32 RadialSteps = 12;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Actions|Creation", meta = (ExposeOnSpawn = "true"))
+	int32 HeightSteps = 4;
+
 
 public:
 	bool Execute_Implementation(bool bEmitRecord) override;
