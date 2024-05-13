fe30e1184f548244ed0e4ff4953a59400a648724
Tobias Pott <mailme@tobiaspott.de>
2024-05-13 22:21:43 +0200

    Added stub for action with confirm dialog
    Changed member categories to default

--- Source/Poly/Public/Functions/AppActions.h
+++ Source/Poly/Public/Functions/AppActions.h
@@ -4,7 +4,31 @@
 
 #include "CoreMinimal.h"
 #include "Actions/ActionBase.h"
+#include "UI/ModalDialogBase.h"
 #include "AppActions.generated.h"
+
+
+
+UCLASS()
+class POLY_API UActionWithConfirmBase : public UActionBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	UActionWithConfirmBase()
+	{
+		Description = "poly.ActionWithConfirm";
+		ShortName = "<Action With Confirm>";
+	}
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default", meta = (ExposeOnSpawn = "true"))
+	TSubclassOf<UModalDialogBase> DialogClass = UModalDialogBase::StaticClass();
+
+	// ToDo: @tpott: GetModalDialog by class, setup (add missing members to base action type) dialog 
+	//					and hook Dialog.Closed event to a lambda which calls the action's Submit or Discard
+	//					REMEMBER: the boolean return of 'Action.Execute' indicates if action is executed (not ssuccess or failure)
+};
 
 /**
  * 
