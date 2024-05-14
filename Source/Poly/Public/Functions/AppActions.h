678c9ba940c320d549e8ef8ef39a3d1155e3b595
Tobias Pott <mailme@tobiaspott.de>
2024-05-14 20:22:09 +0200

    Added acttion with confirm base type
    Update blueprints to include app action collection

--- Source/Poly/Public/Functions/AppActions.h
+++ Source/Poly/Public/Functions/AppActions.h
@@ -25,22 +25,48 @@ public:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default", meta = (ExposeOnSpawn = "true"))
 	TSubclassOf<UModalDialogBase> DialogClass = UModalDialogBase::StaticClass();
 
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default", meta = (ExposeOnSpawn = "true"))
+	FText Title;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default", meta = (ExposeOnSpawn = "true"))
+	FText Subtext;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default", meta = (ExposeOnSpawn = "true"))
+	FString ConfirmLabel = "Yes";
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default", meta = (ExposeOnSpawn = "true"))
+	FString DeclineLabel = "No";
+
 	// ToDo: @tpott: GetModalDialog by class, setup (add missing members to base action type) dialog 
 	//					and hook Dialog.Closed event to a lambda which calls the action's Submit or Discard
-	//					REMEMBER: the boolean return of 'Action.Execute' indicates if action is executed (not ssuccess or failure)
+	//					REMEMBER: the boolean return of 'Action.Execute' indicates if action is executed (not success or failure)
+
+public:
+	bool Execute_Implementation(bool bEmitRecord) override;
+	
+	UFUNCTION()
+	virtual void OnModalDialogClosed(EModalDialogMessage Message);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Default")
+	void OnConfirm();
+	virtual void OnConfirm_Implementation() { this->Submit(); };
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Default")
+	void OnDecline();
+	virtual void OnDecline_Implementation() { this->Discard(); };
+
 };
 
 /**
  * 
  */
 UCLASS()
-class POLY_API UQuitGameActions : public UActionBase
+class POLY_API UQuitGameAction : public UActionBase
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this actor's properties
-	UQuitGameActions()
+	UQuitGameAction()
 	{
 		Description = "poly.QuitGame";
 		ShortName = "Quit Game";
@@ -51,5 +77,26 @@ public:
 
 public:
 	bool Execute_Implementation(bool bEmitRecord) override;
+
+};
+
+UCLASS()
+class POLY_API UQuitGameWithConfirmAction : public UActionWithConfirmBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	UQuitGameWithConfirmAction()
+	{
+		Description = "poly.QuitGameWithConfirm";
+		ShortName = "Quit Game with Confirm";
+	}
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default", meta = (ExposeOnSpawn = "true"))
+	int32 PlayerIndex = 0;
+
+public:
+	void OnConfirm_Implementation() override;
 
 };
